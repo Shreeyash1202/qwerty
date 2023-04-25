8b04a5070d961ec93384fbbc3119b18cafe079b2
Nishant Dixit <nd221619@gmail.com>
2023-04-25 23:17:28 +0530

    navbar changes

--- partial/_header.php
+++ partial/_header.php
@@ -12,27 +12,41 @@ echo '
       <li><a href="info.php">Info</a></li>
       <li><a href="about.php">About</a></li>
     </ul>';
+
+    // Guest
     if(isset($_SESSION["loggedin"])&&$_SESSION["loggedin"]==true&&$_SESSION['role']==0){
       echo'
       <a class="logout-btn" href="partial/_logout.php">Logout</a>
       ';
     }
+    // Coordinator
       elseif(isset($_SESSION["loggedin"])&&$_SESSION["loggedin"]==true&&$_SESSION['role']==1){
         echo'
         <li><a href="report.php">Report</a></li>
         <a class="logout-btn" href="partial/_logout.php">Logout</a>
         ';
       }
+      // Admin HOD
       elseif(isset($_SESSION["loggedin"])&&$_SESSION["loggedin"]==true&&$_SESSION['role']==2){
       echo'
-      <p class ="logintxt">Welcome '.$_SESSION['username'].'</p>
-      <a class="logout-btn" href="partial/_logout.php">Logout</a>
-      ';
+          
+      '
+      // Option to add coordinator
+      ;
      }
       else{
       echo'
       <a class="login-btn" href="partial/_login.php">Login</a>';
     }
+
+    // If logged in show name
+    if(isset($_SESSION["loggedin"])&&$_SESSION["loggedin"]==true){
+      echo'
+      <p class ="logintxt">Welcome '.$_SESSION['username'].'</p>
+      <a class="logout-btn" href="partial/_logout.php">Logout</a>
+      ';
+    }
+
     echo'
 </div>
 </div>
